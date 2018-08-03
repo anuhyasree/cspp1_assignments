ff572d8508c3195b99a1e303688ed8001e7e72c8
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-03 19:52:33 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- Assignment-1/p3/square_root_bisection.py
+++ Assignment-1/p3/square_root_bisection.py
@@ -1,21 +1,20 @@
-# Write a python program to find the square root of the given number
-# using approximation method
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
-
+"""square root of bisection"""
 def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+    """Defining main"""
+    s_s = int(input())
+    eps_ilon = 0.01
+    l_ow = 0
+    h_ih = s_s
+    gu_es = (l_ow+h_ih)/2
+    while abs(gu_es**2-s_s) >= eps_ilon:
+        if gu_es**2 > s_s:
+            h_ih = gu_es
+        else:
+            l_ow = gu_es
+        gu_es = (l_ow+h_ih)/2
+    if (gu_es**2-s_s) >= eps_ilon:
+        print("Failed")
+    else:
+        print(gu_es)
+if __name__ == "__main__":
+    main()
