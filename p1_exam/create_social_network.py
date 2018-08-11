047c928814e370642689012ea7154a35107f54ee
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-11 11:47:59 +0530

    " -> 0 of 3 passed. pylint: 7.65/10 "

--- p1_exam/create_social_network.py
+++ p1_exam/create_social_network.py
@@ -35,7 +35,7 @@ def create_social_network(data):
     # remove the pass below and start writing your code
     d={}
     d = data.split("/n")
-    for i in range (d):
+    for i in range(d):
         d.split("follows")
         d[i[0]] = d[i]
 
