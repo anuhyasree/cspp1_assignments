c9e63a1ec2f94716944c9ffcb27a09de2f631cf5
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-11 11:53:17 +0530

    " -> 0 of 3 passed. pylint: 7.65/10 "

--- p1_exam/create_social_network.py
+++ p1_exam/create_social_network.py
@@ -37,7 +37,7 @@ def create_social_network(data):
     d = data.split("/n")
     for i in data:
         d = data.split("follows")
-        d[i[0]] = d[i]
+        d[i[0]] = d[1]
 
     return data
 
