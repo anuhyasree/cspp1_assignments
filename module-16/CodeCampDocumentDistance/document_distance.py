effb6c4960dd05d82b6dbbb09ea381e39495e291
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-17 14:35:11 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- module-16/CodeCampDocumentDistance/document_distance.py
+++ module-16/CodeCampDocumentDistance/document_distance.py
@@ -21,39 +21,39 @@ def wordlist(input_1):
     return word_freq_dict
 
 def similarity(dict1, dict2):
-   '''
-       Compute the document distance as given in the PDF
-   '''
-   new_dict1 = wordlist(dict1)
-   new_dict2 = wordlist(dict2)
-   stop_words = load_stopwords('stopwords.txt')
-   for word_s in stop_words:
-       if word_s in new_dict1:
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    new_dict1 = wordlist(dict1)
+    new_dict2 = wordlist(dict2)
+    stop_words = load_stopwords('stopwords.txt')
+    for word_s in stop_words:
+        if word_s in new_dict1:
            del new_dict1[word_s]
    for word_s in stop_words:
        if word_s in new_dict2:
-           del new_dict2[word_s]
-   return new_dict1, new_dict2
+            del new_dict2[word_s]
+    return new_dict1, new_dict2
 
 def load_stopwords(filename):
-   '''
-       loads stop words from a file and returns a dictionary
-   '''
-   stopwords = {}
-   with open(filename, 'r') as filename:
-       for line in filename:
-           stopwords[line.strip()] = 0
-       #print(stopwords)
-   return stopwords
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
+    stopwords = {}
+    with open(filename, 'r') as filename:
+        for line in filename:
+            stopwords[line.strip()] = 0
+        #print(stopwords)
+    return stopwords
 
 def main():
-   '''
-       take two inputs and call the similarity function
-   '''
-   input1 = input()
-   input2 = input()
+    '''
+        take two inputs and call the similarity function
+    '''
+    input1 = input()
+    input2 = input()
 
-   print(similarity(input1, input2))
+    print(similarity(input1, input2))
 
 if __name__ == '__main__':
-   main()
+    main()
