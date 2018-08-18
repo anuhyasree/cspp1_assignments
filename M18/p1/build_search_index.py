fc16bb6b5585d8ec944de4c928ab46b3f09888e0
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-18 10:26:17 +0530

    " -> 0 of 1 passed. pylint: 6.92/10 "

--- M18/p1/build_search_index.py
+++ M18/p1/build_search_index.py
@@ -38,7 +38,10 @@ def word_list(text):
         Clean up the text by remvoing all the non alphabet characters
         return a list of words
     '''
-    pass
+    text = text.lower()
+    regex = re.compile('[^a-z ]')
+    text = regex.sub('', text)
+    list_of_words = text.split()
 
 def build_search_index(docs):
     '''
