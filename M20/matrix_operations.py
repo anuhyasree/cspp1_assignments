4715760b72748df64aa1e5b12da14843b302e532
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-23 14:24:51 +0530

    " -> 1 of 8 passed. pylint: 4.57/10 "

--- M20/matrix_operations.py
+++ M20/matrix_operations.py
@@ -9,10 +9,10 @@ def mult_matrix(m1, m2):
     result = []
     if len(m1) == len(m2):
         for i in range(len(m1)):
+            lst = []
             for j in range(len(m1[0])):
-                lst = []
                 sum = 0
-                for k in range(m1):
+                for k in range(len(m2)):
                     sum += int(m1[i][k])*int(m2[k][j])
                 lst.append(sum)
         result.append
