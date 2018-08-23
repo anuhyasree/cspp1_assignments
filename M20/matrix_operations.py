8ee3f637d8814526437b15b605adcabf2a303783
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-23 11:20:23 +0530

    " -> 1 of 8 passed. pylint: 2.92/10 "

---
+++ M20/matrix_operations.py
@@ -0,0 +1,56 @@
+def mult_matrix(m1, m2):
+    '''
+        check if the matrix1 columns = matrix2 rows
+        mult the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for mult
+        and return None
+        error message should be "Error: Matrix shapes invalid for mult"
+    '''
+    pass
+
+def add_matrix(m1, m2):
+    '''
+        check if the matrix shapes are similar
+        add the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for addition
+        and return None
+        error message should be "Error: Matrix shapes invalid for addition"
+    '''
+    pass
+
+def read_matrix():
+    '''
+        read the matrix dimensions from input
+        create a list of lists and read the numbers into it
+        in case there are not enough numbers given in the input
+        print an error message and return None
+        error message should be "Error: Invalid input for the matrix"
+    '''
+    line = input().split(",")
+    r = int(line[0])
+    c = int(line[1])
+    matrix = []
+    for i in range(r):
+        line = input().split(" ")
+        if len(line) == c:
+            matrix.append([int(j) for j in line])
+        else:
+            print("Error: Invalid input for the matrix")
+            return None
+    return matrix
+
+def main():
+    # read matrix 1
+    m1 = read_matrix()
+    print(m1)
+    # read matrix 2
+    m2 = read_matrix()    
+    print(m2)
+
+    # add matrix 1 and matrix 2
+
+    # multiply matrix 1 and matrix 2
+    pass
+
+if __name__ == '__main__':
+    main()
