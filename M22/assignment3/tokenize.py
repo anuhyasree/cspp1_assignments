42532f9d0deb0c88f559c74495b23daa064d2d5f
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-25 11:45:37 +0530

    " -> 0 of 4 passed. pylint: 7.78/10 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -2,13 +2,13 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
+import re
 def tokenize(string):
-    sequence = ''
-    for _ in string:
-        sequence = string.split(' ')
+    string = string.split(' ')
+    regex = re.compile('[^a-z0-9]')
+    return regex.sub('', string)
     freq = {}
-    for i_i in sequence:
+    for i_i in string:
         freq[i_i] = freq.get(i_i, 0) + 1
     return freq
             
