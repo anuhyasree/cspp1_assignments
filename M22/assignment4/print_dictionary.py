b2eacb1f020c2ea48b14d3b68d71e76db937dc11
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-25 12:42:08 +0530

    " -> 0 of 3 passed. pylint: 3.00/10 "

--- M22/assignment4/print_dictionary.py
+++ M22/assignment4/print_dictionary.py
@@ -5,12 +5,14 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
-    keys = list[dictionary.key()]
+    """function"""
+    keys = list[dictionary.keys()]
     keys.sort()
     for key in keys:
-    	print(key,"-", dictionary(key))
+        print(key,"-", dictionary(key))
 
 def main():
+    """main"""
     dictionary = eval(input())
     print_dictionary(dictionary)
 
