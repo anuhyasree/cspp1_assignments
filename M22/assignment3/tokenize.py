5b26ff38f74f02dbb65cd6840c563b566ff3bf39
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-25 11:52:27 +0530

    " -> 0 of 4 passed. pylint: 8.42/10 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -7,6 +7,7 @@ def tokenize(string):
     regex = re.compile('[^a-z A-Z 0-9]')
     string = regex.sub('', string)
     string = string.split(' ')
+    string = string.strip()
     freq = {}
     for i_i in string:
         freq[i_i] = freq.get(i_i, 0) + 1
